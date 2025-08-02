216cf020ee0a4b5d457bd97328a43ac40654f90e
Tuân <nguyenvantuanvp1999@gmail.com>
2025-08-02 21:25:48 +0700

    test commit

--- FreelanceWorkHub/src/main/java/vn/codegym/freelanceworkhub/config/SecurityConfig.java
+++ FreelanceWorkHub/src/main/java/vn/codegym/freelanceworkhub/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package vn.codegym.freelanceworkhub.config;
+bapackage vn.codegym.freelanceworkhub.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
